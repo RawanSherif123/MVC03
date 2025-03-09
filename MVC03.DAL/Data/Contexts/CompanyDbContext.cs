198c7153b98481a3d7908030baaa05e5b732bc3e
HP <HP@DESKTOP-4NLQKFA>
2025-03-10 00:29:53 +0200

    Add project files.

---
+++ MVC03.DAL/Data/Contexts/CompanyDbContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVC03.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC03.DAL.Data.Contexts
+{
+    public class CompanyDbContext : DbContext
+    {
+
+        public CompanyDbContext(DbContextOptions<CompanyDbContext> options) : base(options) 
+        {
+            
+
+        }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer("Server= .; Database = CompanyMVC03; Trusted_Connection = True; TrustServerCertificate= True");
+        //}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet <Department> Departments { get; set; }
+
+    }
+}
---
+++ MVC03.PL/Controllers/DepartmentController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC03.BLL.Interfaces;
+using MVC03.BLL.Repositories;
+using MVC03.DAL.Models;
+using MVC03.PL.Dtos;
+
+
+namespace MVC03.PL.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        private readonly IDepartmentRepository _deptRepository;
+
+        public DepartmentController(IDepartmentRepository departmentRepository)
+        {
+            _deptRepository = departmentRepository;
+        }
+
+        [HttpGet]  // GET ://Department//Index
+        public IActionResult Index()
+        {
+            var departments = _deptRepository.GetAll();
+            return View(departments);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(CreateDepartmentDto model)
+        {
+            if (ModelState.IsValid) // server side validation
+            {
+                var department = new Department()
+                {
+                    Name = model.Name,
+                    Code = model.Code,
+                    CreateAt = model.CreateAt
+                };
+                var count = _deptRepository.Add(department);
+                if (count > 0)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+
+            return View();
+        }
+
+
+        //[HttpGet]
+        //public IActionResult Edit(Department _department)
+        //{
+        //    var deprtment = _deptRepository.Update(_department);
+        //    return View(deprtment);
+        //}
+
+        [HttpGet]
+        public IActionResult Details(int? id )
+        {
+            if (id is null) return BadRequest("Invalid Id ");
+           
+            var deprtment = _deptRepository.Get(id.Value);
+
+            if (deprtment == null) return NotFound(new { statusCode =400 , messege = $"Department With Id:{id} is Not Found" });   
+
+            return View(deprtment);
+        }
+
+    }
+}
